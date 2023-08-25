09f33891cc46474ab5099a28ae2c553434809ea3
Anne Wynter <missannemary1@gmail.com>
2023-08-24 23:14:34 -0400

    changing code in singup handlebar and replacing image

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,33 +1,28 @@
 // Signup request
 const circusSignupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username').value.trim();
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
-  
-    if (username && email && password) {
-      try {
-        const response = await fetch('/api/users/signup', {
-          method: 'POST',
-          body: JSON.stringify({ username, email, password }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-  
-        if (response.ok) {
-          document.location.replace('/'); // When successful, load the homepage
-        } else {
-          alert('Failed to sign up.'); // When unsuccessful, show alert
-        }
-      } catch (error) {
-        console.error('An error occurred while signing up:', error);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector('#username').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/'); // When successful, load the homepage
+    } else {
+      alert('Failed to sign up.'); // When unsuccessful, show alert
     }
-  };
-  
-  // Event listener
-  const circusSignupForm = document.querySelector('#signup-form');
-  if (circusSignupForm) {
-    circusSignupForm.addEventListener('submit', circusSignupFormHandler);
   }
-  
+};
+
+// Event listener
+const circusSignupForm = document.querySelector('#signup-form');
+if (circusSignupForm) {
+  circusSignupForm.addEventListener('submit', circusSignupFormHandler);
+}
