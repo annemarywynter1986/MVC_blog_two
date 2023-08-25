1a9f9e5dc00c6cd715935a7b87a21d2ea9f675cd
Anne Wynter <missannemary1@gmail.com>
2023-08-25 13:51:10 -0400

    fixes

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -1,68 +1,27 @@
-// // Create new post public/js/new-post.js
-// const newCircusPostFormHandler = async (event) => {
-//   event.preventDefault();
-
-//   const title = document.querySelector('#title-new-circus-post').value.trim();
-//   const content = document.querySelector('#content-new-circus-post').value.trim();
-
-
-  
-//   if (title && content) {
-//     const response = await fetch('/api/posts', {
-//       method: 'POST',
-//       body: JSON.stringify({ title, content }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/dashboard'); // When successful, load the dashboard page
-//     } else {
-//       alert('Failed to create a new post.'); // When unsuccessful, show alert
-//     }
-//   }
-// };
-
-// // Event listeners
-// const newCircusPostForm = document.querySelector('.new-circus-post-form');
-// if (newCircusPostForm) {
-//   newCircusPostForm.addEventListener('submit', newCircusPostFormHandler);
-// }
-
+// Create new post public/js/new-post.js
 const newCircusPostFormHandler = async (event) => {
   event.preventDefault();
 
   const title = document.querySelector('#title-new-circus-post').value.trim();
   const content = document.querySelector('#content-new-circus-post').value.trim();
-  const imageInput = document.querySelector('#image-new-circus-post');
-  const imageFile = imageInput.files[0]; // Get the selected image file
 
-  if (title && content && imageFile) {
-    const formData = new FormData();
-    formData.append('title', title);
-    formData.append('content', content);
-    formData.append('image', imageFile);
-
+  if (title && content) {
     const response = await fetch('/api/posts', {
       method: 'POST',
-      body: formData,
+      body: JSON.stringify({ title, content }),
+      headers: { 'Content-Type': 'application/json' },
     });
-   
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/dashboard'); // When successful, load the dashboard page
     } else {
-      alert('Failed to create a new post.');
-      console.log('hello');
+      alert('Failed to create a new post.'); // When unsuccessful, show alert
     }
   }
 };
 
+// Event listeners
 const newCircusPostForm = document.querySelector('.new-circus-post-form');
 if (newCircusPostForm) {
-  newCircusPostForm.addEventListener('submit', (event) => {
-    console.log('Submit button clicked');
-    newCircusPostFormHandler(event);
-  });
+  newCircusPostForm.addEventListener('submit', newCircusPostFormHandler);
 }
-
-
