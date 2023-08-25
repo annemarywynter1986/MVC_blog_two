af835446a294e0b8443dac9d0441eb97eaf6df6b
Anne Wynter <missannemary1@gmail.com>
2023-08-25 13:39:47 -0400

    work on post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
-// Get all posts with associated username
+
 router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
-// Get one post by ID with associated username and comments
+
 router.get("/:id", async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
@@ -25,7 +25,7 @@ router.get("/:id", async (req, res) => {
       ],
     });
     if (!postData) {
-      res.status(404).json({ message: "No post found" });
+      res.status(404).json({ message: "No post found with that id!" });
       return;
     }
     res.status(200).json(postData);
@@ -33,7 +33,7 @@ router.get("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
-// Create a new post with authenticated user
+
 router.post("/", withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
@@ -45,7 +45,7 @@ router.post("/", withAuth, async (req, res) => {
     res.status(400).json(err);
   }
 });
-// Update an existing post with authenticated user
+
 router.put("/:id", withAuth, async (req, res) => {
   try {
     const updatedPost = await Post.update(req.body, {
@@ -53,7 +53,7 @@ router.put("/:id", withAuth, async (req, res) => {
     });
 
     if (!updatedPost) {
-      res.status(404).json({ message: "No post found " });
+      res.status(404).json({ message: "No post found with that id!" });
       return;
     }
     res.status(200).json(updatedPost);
@@ -61,10 +61,9 @@ router.put("/:id", withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-// Delete a post with authenticated user
+
 router.delete("/:id", withAuth, async (req, res) => {
   try {
-    // Delete all comments related to the post
     await Comment.destroy({
       where: { post_id: req.params.id },
     });
@@ -74,7 +73,7 @@ router.delete("/:id", withAuth, async (req, res) => {
     });
 
     if (!deletedPost) {
-      res.status(404).json({ message: "No post found!" });
+      res.status(404).json({ message: "No post found with that id!" });
       return;
     }
     res.status(200).json(deletedPost);
@@ -82,5 +81,5 @@ router.delete("/:id", withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-// Export the router
+
 module.exports = router;
