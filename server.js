157eab229766b1c7218f1cc265ada5c302eb54df
Anne Wynter <missannemary1@gmail.com>
2023-08-24 22:20:35 -0400

    adding dependencies and rewriting server.js

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-const sequelize = require('./configs/connection');
+// Importing required modules and dependencies
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers: require("./utils/helpers") });
 
+// Creating express app and setting port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+// Setting up session object with secret, cookie, and store
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// Using session middleware with session object
+app.use(session(sess));
 
+// Parsing incoming JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
+// IMPORTANT FOR PUBLIC FOLDERS - serving static files such as images from public directory
+app.use(express.static("public"));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Using routes from controller
 app.use(routes);
 
+// Syncing sequelize models with the database and starting the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 });
